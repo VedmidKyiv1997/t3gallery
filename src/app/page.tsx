f321bd2499d949a768d4f766f3aa68bbe0d9f256
Ihor Burdak <burdakigor@gmail.com>
2024-05-22 17:02:49 +0100

    added clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,19 @@
 import { db } from "../server/db";
 
-const mockUrls = [
-  "https://utfs.io/f/d98ce118-91e9-422d-be56-d831a785efe4-skpjf0.jpeg",
-  "https://utfs.io/f/77aa284b-7044-4c96-9fd4-65d55ccfe182-xuptvl.jpeg",
-  "https://utfs.io/f/b3ce0253-9b7d-4c49-9655-9fa30c48cfd3-cyqnyb.jpeg",
-  "https://utfs.io/f/185fbfb4-1053-4c4b-bd29-f6684d61c791-iizduz.jpeg",
-  "https://utfs.io/f/d98ce118-91e9-422d-be56-d831a785efe4-skpjf0.jpeg",
-  "https://utfs.io/f/77aa284b-7044-4c96-9fd4-65d55ccfe182-xuptvl.jpeg",
-  "https://utfs.io/f/b3ce0253-9b7d-4c49-9655-9fa30c48cfd3-cyqnyb.jpeg",
-  "https://utfs.io/f/185fbfb4-1053-4c4b-bd29-f6684d61c791-iizduz.jpeg",
-  "https://utfs.io/f/d98ce118-91e9-422d-be56-d831a785efe4-skpjf0.jpeg",
-  "https://utfs.io/f/77aa284b-7044-4c96-9fd4-65d55ccfe182-xuptvl.jpeg",
-  "https://utfs.io/f/b3ce0253-9b7d-4c49-9655-9fa30c48cfd3-cyqnyb.jpeg",
-  "https://utfs.io/f/185fbfb4-1053-4c4b-bd29-f6684d61c791-iizduz.jpeg",
-  "https://utfs.io/f/d98ce118-91e9-422d-be56-d831a785efe4-skpjf0.jpeg",
-  "https://utfs.io/f/77aa284b-7044-4c96-9fd4-65d55ccfe182-xuptvl.jpeg",
-  "https://utfs.io/f/b3ce0253-9b7d-4c49-9655-9fa30c48cfd3-cyqnyb.jpeg",
-  "https://utfs.io/f/185fbfb4-1053-4c4b-bd29-f6684d61c791-iizduz.jpeg",
-];
+export const dynamic = "force-dynamic";
 
-const mockImages = mockUrls.map((url, idx) => ({
-  id: idx + 1,
-  url,
-}));
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
   return (
     <main className="">
       <div className="flex flex-wrap gap-4 p-4">
-        {mockImages.map((img, i) => (
-          <div key={img.id + "-" + i} className="w-48">
+        {[...images].map((img, i) => (
+          <div key={img.id + "-" + i} className="flex w-48 flex-col">
             <img src={img.url} />
+            <div>{img.name}</div>
           </div>
         ))}
       </div>
